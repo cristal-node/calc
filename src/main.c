84daf4b453ab7c6f37bf12f8218bb8376796771f
cristal-node <12prottoy2002@gmail.com>
2021-06-11 00:29:10 +0900

    char to long double complete, without atoi(math.h)

--- src/main.c
+++ src/main.c
@@ -2,6 +2,7 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#include <math.h>
 
 typedef struct {
     GtkWidget *w_dlg_about;             // Pointer to about dialog box
@@ -75,6 +76,37 @@ void on_0_clicked(GtkButton *button, app_widgets *app_wdgts){
 
     gtk_text_buffer_set_text(app_wdgts->textbuffer_main, text, -1);
 }
+long double ten_power(int n){
+    long double a[6] = {1,10,100,1000,10000,100000};
+    return a[n];
+}
+long double suuji(char *text){
+    int a=0; // bool
+    long double array[9];    // changable
+    // long double num=atoi(text);
+
+    int e=0; // text position
+    long double num=0;
+    for (;text[e]!=0x00;e++){
+        if(text[e]==0x2e){a=1;break;}
+        array[e]=text[e]-'0';
+    }
+    for(int d=0;d<e;++d) // d is array position
+    num+=array[d]*ten_power(e-(d+1));
+
+
+
+    if(a){
+        size_t i =0;
+        for(;text[++e]!=0x00;i++)
+        array[i]=text[e]-'0';
+        for(int d=0;d<i;d++)
+        num+=(array[d]/ten_power(d+1));
+    }
+    printf("num: %Lf\n",num);
+    return num;
+}
+
 
 // void parenthesis(char *text){}
 void plus(char *text){
@@ -98,7 +130,8 @@ void on_eq_clicked(GtkButton *button, app_widgets *app_wdgts){
     gtk_text_buffer_get_end_iter(app_wdgts->textbuffer_main, &end);
     gchar *text = gtk_text_buffer_get_text(app_wdgts->textbuffer_main, &start, &end, FALSE);
 
-    plus(text);
+    // plus(text);
+    suuji(text);
 /* 
     size_t i = 0;
     while (TRUE)
